ed379f0236ad4fd5cb7fd4e585c9136b137f4680
Andrew Anderson <aanderso@tcd.ie>
2013-09-23 18:50:48 +0100

    First program compiled

--- Main.cs
+++ Main.cs
@@ -5,14 +5,14 @@ namespace Tastier { // DMA
 class Tastier { // DMA
 
   public static void Main (string[] arg) {
-    if (arg.Length > 0) {
+    if (arg.Length == 2) {
       Scanner scanner = new Scanner(arg[0]);
       Parser parser = new Parser(scanner);
       parser.Parse();
       if (parser.errors.count == 0) {
-        //parser.Write("test.asm");
+        parser.Write(arg[1]);
       }
-    } else Console.WriteLine("-- No source file specified");
+    } else Console.WriteLine("Usage: tcc <program.TAS> <output.asm>");
   }
 
 }
